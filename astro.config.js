10feb22bd119f88bf29f3dc4465fd6800d3a5c11
Zen <muhzaini30@gmail.com>
2023-06-14 07:33:07 +0800

    Update astro.config.js

--- astro.config.js
+++ astro.config.js
@@ -10,7 +10,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://localhost',
+  site: 'https://jasazen.my.id',
   integrations: [UnoCSS({
     injectReset: true,
     // or a path to the reset file
